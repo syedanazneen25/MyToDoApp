3cc1d6e2aedc194c07029a9ed349e0c2afad3a79
nazneensyeda31 <nazneensyeda31@gmail.com>
2026-01-08 20:19:56 -0700

    Initial Commit

---
+++ MyToDo/src/main/java/com/nazneen/MyToDo/MyToDoApplication.java
@@ -0,0 +1,13 @@
+package com.nazneen.MyToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyToDoApplication.class, args);
+	}
+
+}
---
+++ MyToDo/src/test/java/com/nazneen/MyToDo/MyToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazneen.MyToDo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
